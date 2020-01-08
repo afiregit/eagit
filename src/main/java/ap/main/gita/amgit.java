126fe79c6bcda82c45fb734ec9338a5bb89c266a
afiregit <afire.lee@gmail.com>
2020-01-08 11:49:58 +0800

    First Commit

---
+++ src/main/java/ap/main/gita/amgit.java
@@ -0,0 +1,10 @@
+package ap.main.gita;
+
+public class amgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Main World!" );
+	}
+
+}
---
+++ src/test/java/ap/test/gitz/atgit.java
@@ -0,0 +1,10 @@
+package ap.test.gitz;
+
+public class atgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Test  World!" );
+	}
+
+}
