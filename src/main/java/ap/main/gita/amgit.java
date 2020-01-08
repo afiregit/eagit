c0f82602bc3298b88c8617de82176f5f98840e7b
afiregit <afire.lee@gmail.com>
2020-01-08 14:37:32 +0800

    Hot Fix

--- src/main/java/ap/main/gita/amgit.java
+++ src/main/java/ap/main/gita/amgit.java
@@ -5,6 +5,7 @@ public class amgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println( "Main World!" );
+		System.out.println( "Main Hotfix!" );
 	}
 
 }
